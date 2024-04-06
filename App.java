da050f96c0bd576007cba79e95ea7ec05f984eda
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2024-04-06 11:21:38 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    // package aulas;
+
+    //import java.util.Scanner; // import= importa, java.util= pacote com classe, Scanner= e a classe no apcote, vem do jdk, 
+    //sem ele nao programa em java
+
+    //import java.util*; o asterisco faz cm q tds classe no java.util seja importada
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
